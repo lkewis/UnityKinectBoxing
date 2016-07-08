0b93822559fbdabe63eb70119e448c3765bcbef3
lkewis <lewis.hackett@gmail.com>
2016-07-08 13:00:09 +0100

    First commit

---
+++ Assets/Script/HandCollide.js
@@ -0,0 +1,7 @@
+﻿function OnCollisionEnter(theCollision : Collision){
+
+	if(theCollision.gameObject.name == "Cube"){
+		Debug.Log("Hit the cube");
+	}
+
+}
---
+++ Assets/Script/Movement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x--;
+			this.transform.position = position;
+		}
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x++;
+			this.transform.position = position;
+		}
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.y++;
+			this.transform.position = position;
+		}
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.y--;
+			this.transform.position = position;
+		}
+	}
+}
